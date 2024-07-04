5ef9d4bbf5198bc4924b00bec02d9d773fc3c2a2
pothmann <peter.pothmann@ufz.de>
2024-07-04 15:19:00 +0200

    change spelling

--- code/plot/model_type.R
+++ code/plot/model_type.R
@@ -18,19 +18,19 @@ modelTypeTable <- reviewTable |>
   select(modelType) |>
   separate_rows(modelType, sep = ',') |>
   mutate(modelType = trimws(modelType),
-         modelType = tolower(modelType),
+         #modelType = tolower(modelType),
          modelType = case_when(modelType == "random forests" ~ "random forest",
-                               modelType == "logisitic regression model" ~ "logistic regression",
+                               modelType == "Logisitic regression model" ~ "Logistic regression",
                                .default = modelType))
 
 modelTypeTable <- modelTypeTable %>%
   mutate(modelClass = case_when(
-    grepl("maxent", modelType) ~ "Maxent",
-    grepl("logistic regression|generalized linear mixed model", modelType) ~ "regression models",
-    grepl("boosted regression trees|random forest|xgboost", modelType) ~ "tree-based models",
-    grepl("polynomial distributed lag models|non linear discriminant analysis|garp", modelType) ~ "specialized models",
-    grepl("custom|zpoms", modelType) ~ "Custom models",
-    grepl("spde|simple kriging", modelType) ~ "geostatistic models"
+    grepl("Maxent", modelType) ~ "Maxent",
+    grepl("Logistic regression|Generalized linear mixed model", modelType) ~ "Regression models",
+    grepl("Boosted Regression Trees|Random forest|XGBoost", modelType) ~ "Tree-based models",
+    grepl("Polynomial distributed lag models|Non Linear Discriminant Analysis|Genetic Algorithm for Rule Set Production", modelType) ~ "Specialized models",
+    grepl("Custom|ZPOMs", modelType) ~ "Custom models",
+    grepl("Stochastic partial differential equation|Simple kriging", modelType) ~ "geostatistic models"
   ))
 
 modelTypePlotTable <- modelTypeTable |> 
@@ -41,8 +41,8 @@ modelTypePlotTable$modelClass <- reorder(modelTypePlotTable$modelClass, modelTyp
 
 ggplot(data = modelTypePlotTable, aes(x = modelClass, y = n, fill = modelType, label = modelType)) +
   labs(title =  element_text(paste0("Total number: ", sum(modelTypePlotTable$n)))) + 
-  ylab("Count")+
-  xlab("model type") + 
+  ylab("Frequency of models applied")+
+  xlab("Model type") + 
   geom_bar(position = "stack", stat = "identity") + 
   # geom_text(aes(x = modelClass, y = n, label = modelType),
   #           position = position_stack(vjust = .5), size = 3.9) + 
@@ -51,12 +51,12 @@ ggplot(data = modelTypePlotTable, aes(x = modelClass, y = n, fill = modelType, l
   theme_bw(base_size = 16) +
   theme(legend.position="none")
 
-s <- sum(modelTypePlotTable$n)
+sumModelTypes <- sum(modelTypePlotTable$n)
 
 # get percentage of the model type
 metricsModelType <- modelTypePlotTable |> 
-  mutate(perc = n / s * 100)
+  mutate(perc = n / sumModelTypes * 100)
 
 metricsModelClass <- modelTypePlotTable |> 
   group_by(modelClass) |> 
-  summarise(perc = sum(n) / s * 100)
+  summarise(perc = sum(n) / sumModelTypes * 100)
